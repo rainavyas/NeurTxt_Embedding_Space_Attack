1f5410fddc1457a2284591798ee5340f3a98581c
vyas raina <sayvaniar@gmail.com>
2021-01-28 13:11:04 +0000

    bertify and everything

--- generate_universal_variance_attacks.py
+++ generate_universal_variance_attacks.py
@@ -22,7 +22,7 @@ def apply_attack(X, attack_direction, epsilon):
     return X_attacked
 
 commandLineParser = argparse.ArgumentParser()
-commandLineParser.add_argument('DATA', type=str, help='Specify input pkl file with prepared useful data')
+commandLineParser.add_argument('DATA', type=str, help='Specify input txt file with prepared useful data')
 commandLineParser.add_argument('MODEL', type=str, help='Specify trained model path')
 commandLineParser.add_argument('GRADES', type=str, help='Specify grades file')
 commandLineParser.add_argument('--epsilon', default=1.0, type=float, help='Specify the epsilon constraint')
@@ -39,13 +39,49 @@ if not os.path.isdir('CMDs'):
 with open('CMDs/generate_universal_variance_attacks.cmd', 'a') as f:
     f.write(' '.join(sys.argv)+'\n')
 
-# Load the data
-pkl = pickle.load(open(data_file, "rb"))
-X = pkl['X']
-M = pkl['M']
-speaker_ids = pkl['ids']
+MAX_UTTS_PER_SPEAKER_PART = 1
+MAX_WORDS_IN_UTT = 200
 
-# Get the grades
+# Load the data
+with open(data_file, 'r') as f:
+    utterances = json.loads(f.read())
+print("Loaded Data")
+
+# Convert json output from unicode to string
+utterances = [[str(item[0]), str(item[1])] for item in utterances]
+
+# Load tokenizer and BERT model
+tokenizer = BertTokenizer.from_pretrained('bert-base-cased', do_basic_tokenize=False, do_lower_case=True)
+bert_model = BertModel.from_pretrained('bert-base-cased')
+bert_model.eval()
+print("Loaded BERT model")
+
+# Convert sentences to a list of BERT embeddings (embeddings per word)
+# Store as dict of speaker id to utterances list (each utterance a list of embeddings)
+utt_embs = {}
+
+for item in utterances:
+    fileName = item[0]
+    speakerid = fileName[:12]
+    sentence = item[1]
+
+    tokenized_text = tokenizer.tokenize(sentence)
+    indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
+    if len(indexed_tokens) < 1:
+        word_vecs = [[0]*768]
+    else:
+        tokens_tensor = torch.tensor([indexed_tokens])
+        with torch.no_grad():
+            outputs = bert_model(tokens_tensor)
+            encoded_layers = outputs.last_hidden_state
+            #encoded_layers, _ = bert_model(tokens_tensor)
+        bert_embs = encoded_layers.squeeze(0)
+        word_vecs = bert_embs.tolist()
+    if speakerid not in utt_embs:
+        utt_embs[speakerid] =  [word_vecs]
+    else:
+        utt_embs[speakerid].append(word_vecs)
+
 # get speakerid to section grade dict
 grade_dict = {}
 section_num = 3
@@ -64,15 +100,69 @@ for line in lines[1:]:
         grade = float(grades[section_num-1])
         grade_dict[speaker_id] = grade
 
+
 # Create list of grades and speaker utterance in same speaker order
 grades = []
-for id in speaker_ids:
-    grades.append(grade_dict[id])
+vals = []
+
+for id in utt_embs:
+    try:
+        grades.append(grade_dict[id])
+        vals.append(utt_embs[id])
+    except:
+        print(id)
+
+# Convert to appropriate 4D tensor
+# Initialise list to hold all input data in tensor format
+X = []
+y = []
+
+# Initialise 2D matrix format to store all utterance lengths per speaker
+utt_lengths_matrix = []
+
+for utts, grade in zip(vals, grades):
+        new_utts = []
+
+        # Reject speakers with not exactly correct number of utterances in part
+        if len(utts) != MAX_UTTS_PER_SPEAKER_PART:
+                continue
 
 
+        # Create list to store utterance lengths
+        utt_lengths = []
+
+        for curr_utt in utts:
+                num_words = len(curr_utt)
+
+                if num_words <= MAX_WORDS_IN_UTT:
+                        # append padding of zero vectors
+                        words_to_add = MAX_WORDS_IN_UTT - num_words
+                        zero_vec_word = [0]*768
+                        zero_vec_words = [zero_vec_word]*words_to_add
+                        new_utt = curr_utt + zero_vec_words
+                        utt_lengths.append(num_words)
+                else:
+                        # Shorten utterance from end
+                        new_utt = curr_utt[:MAX_WORDS_IN_UTT]
+                        utt_lengths.append(MAX_WORDS_IN_UTT)
+
+                # Convert all values to float
+                new_utt = [[float(i) for i in word] for word in new_utt]
+
+                new_utts.append(new_utt)
+
+        X.append(new_utts)
+        y.append(grade)
+        utt_lengths_matrix.append(utt_lengths)
+
+# Convert to tensors
 X = torch.FloatTensor(X)
+y = torch.FloatTensor(y)
+L = torch.FloatTensor(utt_lengths_matrix)
+
+# Make the mask from utterance lengths matrix L
+M = [[([1]*utt_len + [-100000]*(X.size(2)- utt_len)) for utt_len in speaker] for speaker in utt_lengths_matrix]
 M = torch.FloatTensor(M)
-y = torch.FloatTensor(grades)
 
 # Load the trained model
 trained_model = torch.load(trained_model_path)
